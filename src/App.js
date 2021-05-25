3890c3bb5a9ddf745e8982ddc7aa5f93e35fa6f6
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2021-05-25 13:20:55 +0200

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import DefaultLayout from './layout/DefaultLayout';
 import { Entry } from './pages/entry/Entry.page';
 import "./App.css"
+import Dashboard from './pages/dashboard/Dashboard.page';
 
 function App() {
     return ( < div className = "App" > { /*<Entry/>*/ } 
     <DefaultLayout>
-        Send page component
+        <Dashboard/>
     </DefaultLayout>
         </div>
     );
